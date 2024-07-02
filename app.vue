a6062531e8b9c28a6088853994c17ff97094d88b
KentChen <bboykunz@gmail.com>
2024-07-02 11:17:32 +0800

    Construct base layout in app.vue(just one layout)

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 
 <!-- import PrimaryButton from './components/PrimaryButton.vue'; -->
 <template>
+  <div class="navbar">
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Contact</a>
+  </div>
   <div style="display: flex; gap: 8px; align-items: center; justify-content: center;">
     <PrimaryButton />
     <SecondaryButton />
@@ -10,7 +16,20 @@
     <NuxtWelcome />
   </div>
 </template>
-<!-- 
-<style lang="scss">
-@use "~/assets/scss/components.scss";
-</style> -->
+
+<style lang="sass" scoped>
+.navbar 
+  display: flex
+  justify-content: space-between
+  padding: $large-padding
+  border-bottom: 2px solid $back-color
+
+  a
+    text-decoration: none
+    color: $back-color
+    font-weight: bold
+    padding: $small-padding $large-padding
+
+    &:hover
+      color: $blue-color
+</style>
