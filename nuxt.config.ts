2f05372598d21c3a23b26b7f4ada67c82fe5bea7
KentChen <bboykunz@gmail.com>
2024-07-02 10:49:03 +0800

    Use styleHook.sass to construct actual style class

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  // css: [
-  //   "~/assets/style/components.sass"
-  // ],
+  css: [
+    "~/assets/style/styleHook.sass"
+  ],
   vite: {
     css: {
       preprocessorOptions: {
