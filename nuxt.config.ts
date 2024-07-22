2b71a852a25e1918b70cbfc472ddf345f564b746
KentChen <bboykunz@gmail.com>
2024-07-22 17:45:12 +0800

    import and setting tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: [
-    "~/assets/style/styleHook.sass"
-  ],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        sass: {
-          additionalData: '@use "~/assets/style/components.sass" as *\n'
-        }
-      }
-    }
-  }
+  buildModules: ['@nuxtjs/tailwindcss'],
+  // css: [
+  //   "~/assets/style/styleHook.sass"
+  // ],
+  // vite: {
+  //   css: {
+  //     preprocessorOptions: {
+  //       sass: {
+  //         additionalData: '@use "~/assets/style/components.sass" as *\n'
+  //       }
+  //     }
+  //   }
+  // }
 })
