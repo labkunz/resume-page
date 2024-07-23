a26ad846f23d31122a91d3dac0da19d3bfa77776
KentChen <bboykunz@gmail.com>
2024-07-23 19:30:00 +0800

    Add image layout

--- app.vue
+++ app.vue
@@ -3,6 +3,16 @@
     <PrimaryButton />
     <SecondaryButton />
   </div>
+  <section>
+    info
+    <div>picture</div>
+    <div>contact</div>
+    <div>name</div>
+  </section>
+  <section>about myself</section>
+  <section>skills</section>
+  <section>work experience</section>
+  <section>education</section>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
