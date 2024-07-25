fb6d8fa047fa66c11c85d887e1656e1a6c002370
kunz <bboykunz@gmail.com>
2024-07-26 01:28:53 +0800

    Update app.vue to use semantic element to layout page

--- app.vue
+++ app.vue
@@ -3,18 +3,87 @@
     <PrimaryButton />
     <SecondaryButton />
   </div>
-  <section>
-    info
-    <div>picture</div>
-    <div>contact</div>
-    <div>name</div>
-  </section>
-  <section>about myself</section>
-  <section>skills</section>
-  <section>work experience</section>
-  <section>education</section>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main>
+    <aside>
+      <figure>
+        <img src="" alt="">
+      </figure>
+      <section>
+        <h2>MY CONTACT</h2>
+        <hgroup>
+          <h3>Phone</h3>
+          <p>0912345678</p>
+        </hgroup>
+        <hgroup>
+          <h3>Email</h3>
+          <p>example@gmail.com</p>
+        </hgroup>
+        <hgroup>
+          <h3>Address</h3>
+          <p>4654564564646</p>
+        </hgroup>
+      </section>
+      <section>
+        <h2>SKILLS</h2>
+        <ul>
+          <li>Html</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Vue</li>
+          <li>Nuxt</li>
+          <li>React</li>
+          <li>Scss</li>
+          <li>tailwind css</li>
+          <li>RWD</li>
+          <li>Bootstrap</li>
+        </ul>
+      </section>
+    </aside>
+    <section>
+      <header>
+        <hgroup>
+          <h1>Kent Chen</h1>
+          <p>Front-end engineer</p>
+        </hgroup>
+      </header>
+      <article>
+        <h2>ABOUT MYSELF</h2>
+        <p></p>
+      </article>
+      <article>
+        <h2>EXPERIENCE</h2>
+        <div class="work-e-item">
+          <hgroup>
+            <h3></h3>
+            <p></p>
+          </hgroup>
+          <p></p>
+        </div>
+        <div class="work-e-item">
+          <hgroup>
+            <h3></h3>
+            <p></p>
+          </hgroup>
+          <p></p>
+        </div>
+      </article>
+      <article>
+        <h2>EDUCATION</h2>
+        <div class="school-e-item">
+          <hgroup>
+            <h3></h3>
+            <p></p>
+          </hgroup>
+          <p></p>
+        </div>
+        <div class="school-e-item">
+          <hgroup>
+            <h3></h3>
+            <p></p>
+          </hgroup>
+          <p></p>
+        </div>
+      </article>
+    </section>
+  </main>
 </template>
